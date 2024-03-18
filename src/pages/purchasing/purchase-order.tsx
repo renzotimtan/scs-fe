a08996c784dab9ff1c56f31f4097ff711fe2b1e5
Clynt Uy <clynt.timothy.uy@gmail.com>
2024-03-18 23:41:43 +0800

    Started PO - only read
    - create, update, and delete TBD

--- src/pages/purchasing/purchase-order.tsx
+++ src/pages/purchasing/purchase-order.tsx
@@ -10,14 +10,93 @@ import {
   Option,
   Box,
   Divider,
+  Autocomplete,
 } from "@mui/joy";
 import LocalPrintshopIcon from "@mui/icons-material/LocalPrintshop";
 import SaveIcon from "@mui/icons-material/Save";
 import DoDisturbIcon from "@mui/icons-material/DoDisturb";
-import FileDownloadIcon from "@mui/icons-material/FileDownload";
 import Table from "@mui/joy/Table";
+import { useEffect, useState } from "react";
+import axiosInstance from "../../utils/axiosConfig";
+
+interface Supplier {
+  supplier_id: number;
+  name: string;
+}
+
+interface Item {
+  id: number;
+  stock_code: string;
+  name: string;
+  category: string;
+  brand: string;
+  acquisition_cost: number;
+  currency: string;
+  rate: number;
+  total_on_stock: number;
+  total_available: number;
+  total_allocated: number;
+  total_purchased: number;
+  total_sold: number;
+  total_reorder_level: number;
+  total_unserved_cpo: number;
+  total_unserved_spo: number;
+  creator: {
+    full_name: string;
+    username: string;
+    email: string;
+    id: number;
+  };
+  date_created: string;
+  modifier: {
+    full_name: string;
+    username: string;
+    email: string;
+    id: number;
+  };
+  date_modified: string;
+}
 
 const PurchaseOrderForm = (): JSX.Element => {
+  const [suppliers, setSuppliers] = useState<Supplier[]>([]);
+  const [selectedSupplier, setSelectedSupplier] = useState<Supplier | null>(
+    null,
+  );
+  const [items, setItems] = useState<Item[]>([]);
+  const [currencyUsed, setCurrencyUsed] = useState<string>("USD");
+  const [supplierDiscount, setSupplierDiscount] = useState<number>(0);
+  const [transactionDiscount, setTransactionDiscount] = useState<number>(0);
+  const [pesoRate, setPesoRate] = useState<number>(56);
+
+  useEffect(() => {
+    // Fetch suppliers
+    axiosInstance
+      .get<Supplier[]>("/api/suppliers/")
+      .then((response) => setSuppliers(response.data))
+      .catch((error) => console.error("Error:", error));
+  }, []);
+
+  useEffect(() => {
+    if (selectedSupplier !== null) {
+      // Fetch items for the selected supplier
+      axiosInstance
+        .get<Item[]>(`/api/items?supplier_id=${selectedSupplier.supplier_id}`)
+        .then((response) => setItems(response.data))
+        .catch((error) => console.error("Error:", error));
+    }
+  }, [selectedSupplier]);
+
+  // Calculations
+  const fobTotal = items.reduce(
+    (acc, item) => acc + item.total_available * item.acquisition_cost,
+    0,
+  );
+  const supplierDiscountAmount = (supplierDiscount / 100) * fobTotal;
+  const transactionDiscountAmount = (transactionDiscount / 100) * fobTotal;
+  const netAmount =
+    fobTotal - supplierDiscountAmount - transactionDiscountAmount;
+  const landedTotal = netAmount * pesoRate;
+
   return (
     <div>
       <h2 className="mb-6">Create Purchase Order</h2>
@@ -42,15 +121,17 @@ const PurchaseOrderForm = (): JSX.Element => {
             <FormControl size="sm" sx={{ mb: 1, mt: 2 }}>
               <FormLabel>Supplier</FormLabel>
               <div className="flex">
-                <Input
+                <Autocomplete
+                  options={suppliers}
+                  getOptionLabel={(option) => option.name}
+                  value={selectedSupplier}
+                  onChange={(event, newValue) => {
+                    setSelectedSupplier(newValue);
+                  }}
                   size="sm"
-                  placeholder="ABC Corporation"
-                  className="w-[85%]"
-                  disabled
+                  className="w-[100%]"
+                  placeholder="Select Supplier"
                 />
-                <Button className="ml-4 w-[130px] bg-button-primary flex">
-                  <FileDownloadIcon className="mr-2" /> Select
-                </Button>
               </div>
             </FormControl>
             <Stack direction="row" spacing={2} sx={{ mb: 1 }}>
@@ -59,11 +140,36 @@ const PurchaseOrderForm = (): JSX.Element => {
                 <Select size="sm" placeholder="Unposted">
                   <Option value="unposted">Unposted</Option>
                   <Option value="posted">Posted</Option>
+                  <Option value="posted">Cancelled</Option>
                 </Select>
               </FormControl>
               <FormControl size="sm" sx={{ mb: 1, width: "48%" }}>
                 <FormLabel>Transaction Date</FormLabel>
                 <Input type="date" />
+              </FormControl>
+            </Stack>
+            <Stack direction="row" spacing={2} sx={{ mb: 1 }}>
+              <FormControl size="sm" sx={{ mb: 1, width: "48%" }}>
+                <FormLabel>Supplier Discount</FormLabel>
+                <Input
+                  endDecorator="%"
+                  size="sm"
+                  placeholder="55"
+                  value={supplierDiscount}
+                  onChange={(e) => setSupplierDiscount(Number(e.target.value))}
+                />
+              </FormControl>
+              <FormControl size="sm" sx={{ mb: 1, width: "48%" }}>
+                <FormLabel>Transaction Discount</FormLabel>
+                <Input
+                  endDecorator="%"
+                  size="sm"
+                  placeholder="55"
+                  value={transactionDiscount}
+                  onChange={(e) =>
+                    setTransactionDiscount(Number(e.target.value))
+                  }
+                />
               </FormControl>
             </Stack>
             <Stack direction="row" spacing={2} sx={{ mb: 1 }}>
@@ -76,17 +182,13 @@ const PurchaseOrderForm = (): JSX.Element => {
               </FormControl>
               <FormControl size="sm" sx={{ mb: 1, width: "48%" }}>
                 <FormLabel>Philippine Peso Rate</FormLabel>
-                <Input startDecorator="₱" size="sm" placeholder="55" />
-              </FormControl>
-            </Stack>
-            <Stack direction="row" spacing={2} sx={{ mb: 1 }}>
-              <FormControl size="sm" sx={{ mb: 1, width: "48%" }}>
-                <FormLabel>Supplier Discount</FormLabel>
-                <Input startDecorator="₱" size="sm" placeholder="55" />
-              </FormControl>
-              <FormControl size="sm" sx={{ mb: 1, width: "48%" }}>
-                <FormLabel>Transaction Discount</FormLabel>
-                <Input startDecorator="₱" size="sm" placeholder="55" />
+                <Input
+                  startDecorator="₱"
+                  size="sm"
+                  placeholder="55"
+                  value={pesoRate}
+                  onChange={(e) => setPesoRate(Number(e.target.value))}
+                />
               </FormControl>
             </Stack>
           </div>
@@ -96,15 +198,15 @@ const PurchaseOrderForm = (): JSX.Element => {
             <div className="flex justify-around">
               <FormControl size="sm" sx={{ mb: 1 }}>
                 <FormLabel>FOB Total</FormLabel>
-                <h5>₱15,000</h5>
+                <h5>₱{fobTotal.toFixed(2)}</h5>{" "}
               </FormControl>
               <FormControl size="sm" sx={{ mb: 1 }}>
                 <FormLabel>NET Amount</FormLabel>
-                <h5>₱15,000</h5>
+                <h5>₱{netAmount.toFixed(2)}</h5>
               </FormControl>
               <FormControl size="sm" sx={{ mb: 1 }}>
                 <FormLabel>LANDED Total</FormLabel>
-                <h5>₱15,000</h5>
+                <h5>₱{landedTotal.toFixed(2)}</h5>
               </FormControl>
             </div>
             <Divider />
@@ -143,157 +245,35 @@ const PurchaseOrderForm = (): JSX.Element => {
         <Table className="mt-6">
           <thead>
             <tr>
-              <th style={{ width: "15%" }}>Stock</th>
-              <th style={{ width: "20%" }}>Product Name</th>
+              <th>Stock Code</th>
+              <th>Name</th>
               <th>Current Price</th>
               <th>Volume</th>
               <th>Price</th>
               <th>Gross</th>
               <th>Created By</th>
-              <th style={{ width: "15%" }}>Date Created</th>
+              <th>Date Created</th>
+              <th>Modified By</th>
+              <th>Date Modified</th>
             </tr>
           </thead>
           <tbody>
-            <tr>
-              <td>
-                <FileDownloadIcon className="mr-2 cursor-pointer" />
-                MCRO1532
-              </td>
-              <td>TAPE 1 IN.</td>
-              <td>159.0</td>
-              <td>
-                <Input
-                  size="sm"
-                  className="w-10/12"
-                  type="number"
-                  slotProps={{
-                    input: {
-                      min: 0,
-                    },
-                  }}
-                />
-              </td>
-              <td>
-                <Input
-                  size="sm"
-                  className="w-10/12"
-                  type="number"
-                  slotProps={{
-                    input: {
-                      min: 0,
-                    },
-                  }}
-                />
-              </td>
-              <td>1590.0</td>
-              <td>RENZO</td>
-              <td>01/29/2024 11:55 AM</td>
-            </tr>
-            <tr>
-              <td>
-                <FileDownloadIcon className="mr-2 cursor-pointer" />
-                MCRO1532
-              </td>
-              <td>TAPE 1 IN.</td>
-              <td>159.0</td>
-              <td>
-                <Input
-                  size="sm"
-                  className="w-10/12"
-                  type="number"
-                  slotProps={{
-                    input: {
-                      min: 0,
-                    },
-                  }}
-                />
-              </td>
-              <td>
-                <Input
-                  size="sm"
-                  className="w-10/12"
-                  type="number"
-                  slotProps={{
-                    input: {
-                      min: 0,
-                    },
-                  }}
-                />
-              </td>
-              <td>1590.0</td>
-              <td>RENZO</td>
-              <td>01/29/2024 11:55 AM</td>
-            </tr>
-            <tr>
-              <td>
-                <FileDownloadIcon className="mr-2 cursor-pointer" />
-                MCRO1532
-              </td>
-              <td>TAPE 1 IN.</td>
-              <td>159.0</td>
-              <td>
-                <Input
-                  size="sm"
-                  className="w-10/12"
-                  type="number"
-                  slotProps={{
-                    input: {
-                      min: 0,
-                    },
-                  }}
-                />
-              </td>
-              <td>
-                <Input
-                  size="sm"
-                  className="w-10/12"
-                  type="number"
-                  slotProps={{
-                    input: {
-                      min: 0,
-                    },
-                  }}
-                />
-              </td>
-              <td>1590.0</td>
-              <td>RENZO</td>
-              <td>01/29/2024 11:55 AM</td>
-            </tr>
-            <tr>
-              <td>
-                <FileDownloadIcon className="mr-2 cursor-pointer" />
-                MCRO1532
-              </td>
-              <td>TAPE 1 IN.</td>
-              <td>159.0</td>
-              <td>
-                <Input
-                  size="sm"
-                  className="w-10/12"
-                  type="number"
-                  slotProps={{
-                    input: {
-                      min: 0,
-                    },
-                  }}
-                />
-              </td>
-              <td>
-                <Input
-                  size="sm"
-                  className="w-10/12"
-                  type="number"
-                  slotProps={{
-                    input: {
-                      min: 0,
-                    },
-                  }}
-                />
-              </td>
-              <td>1590.0</td>
-              <td>RENZO</td>
-              <td>01/29/2024 11:55 AM</td>
-            </tr>
+            {items.map((item) => (
+              <tr key={item.id}>
+                <td>{item.stock_code}</td>
+                <td>{item.name}</td>
+                <td>{item.acquisition_cost}</td>
+                <td>{item.total_on_stock}</td>
+                <td>{item.acquisition_cost}</td>
+                <td>
+                  {Number(item.total_available) * Number(item.acquisition_cost)}
+                </td>
+                <td>{item.creator.username}</td>
+                <td>{item.date_created}</td>
+                <td>{item.modifier?.username}</td>
+                <td>{item?.date_modified}</td>
+              </tr>
+            ))}
           </tbody>
         </Table>
         <Divider />
